55699d2a6bd04a3b010048b030f55cf6c2895339
dj4t9n <pandeyjatin47@gmail.com>
2021-10-02 15:37:56 +0530

    wrote test for gray image conversion

--- tests/opencv_helper.py
+++ tests/opencv_helper.py
@@ -1,1 +1,8 @@
+from vimotrack.lib import detector
+import cv2
 
+def test_gray():
+    img_path = "./assets/test.png"
+    img = cv2.imread(img_path)
+    gray = detector.apply_gray(img)
+    assert(gray, cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
